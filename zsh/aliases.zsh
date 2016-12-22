aee597b3297e7f036c682d63194e50f6162ea0d6
Brian Skjerven <brian.skjerven@gmail.com>
2016-12-22 11:32:03 +0800

    Updated git alias to hub, moved noglob alias to alias file.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -75,6 +75,9 @@ alias svi='sudo vi'
 alias ze='vim ~/.zshrc'
 
 # Git Aliases
+# Don't try to glob with zsh so you can do
+# stuff like ga *foo* and correctly have
+# git add the right stuff
 alias git='noglob hub'
 alias gs='git status'
 alias gstsh='git stash'
