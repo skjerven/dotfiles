5e668cb82e4c977e18ce6bff98e2b75188f17f9e
Brian Skjerven <brian.skjerven@gmail.com>
2017-07-25 14:19:34 +0800

    Added python to path to fix Homebrew's changes

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -4,3 +4,6 @@ is_osx || return 1
 # Some fuckheads decided to change coreutils, so now we have to do this
 # in order to get rid of quotes in ls
 export QUOTING_STYLE=literal
+
+# Homebrew changed how python executables are named
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
