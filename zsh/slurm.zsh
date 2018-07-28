da024ff2ae6280df3be4bc2efb81c709c8395232
Brian Skjerven <brian.skjerven@gmail.com>
2018-07-28 21:28:34 +0800

    Added OS test for SLURM zsh functions

--- zsh/slurm.zsh
+++ zsh/slurm.zsh
@@ -1,2 +1,4 @@
-autoload -U +X bashcompinit && bashcompinit
-bash_source /group/pawsey0001/bskjerven/etc/slurm_completion.sh
+if ! is_osx; then
+  autoload -U +X bashcompinit && bashcompinit
+  bash_source /group/pawsey0001/bskjerven/etc/slurm_completion.sh
+fi
