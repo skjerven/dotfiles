fdb53992681e3d6884bf044fe25d1dd13f75de67
Brian Skjerven <brian.skjerven@gmail.com>
2019-06-05 12:18:37 +0800

    Modified pip-update function to exclude updating pip
    We let homebrew update pip to avoid breaking things

--- zsh/pip-update.zsh
+++ zsh/pip-update.zsh
@@ -1,12 +1,9 @@
 function pip-update() {
 
   if is_osx; then
-    #Update pip
     echo ""
-    echo "Updating pip & pip packages"
-    #pip install --upgrade pip
-    pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install --no-cache-dir -U
-    pip_upgrade_outdated --pip_cmd pip
+    echo "Updating pip packages"
+    pip_upgrade_outdated --pip_cmd pip --exclude pip
     echo ""
   fi
 }
