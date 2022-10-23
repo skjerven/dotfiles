7bebda1a4e01832a15ffda59a9ea4d75048785e6
Brian Skjerven <brian.skjerven@gmail.com>
2022-10-23 10:25:48 +0200

    Check host for AWS shell commands

--- zsh/aws.zsh
+++ zsh/aws.zsh
@@ -3,4 +3,6 @@ alias cfn-nag-rules='cfn_nag_rules'
 alias cfn-nag-scan='cfn_nag_scan'
 alias isengard='isengardcli'
 
-eval "$(isengardcli shell-autocomplete)"
+if [[ $(hostname) == "147dda49eefc.ant.amazon.com" ]]; then
+  eval "$(isengardcli shell-autocomplete)"
+fi
