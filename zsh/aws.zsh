1e36a10992bbf0461c7613a215c290924b308f48
Brian Skjerven <brian.skjerven@gmail.com>
2023-06-05 08:12:08 +0100

    Midway alias added to shell functions

--- zsh/aws.zsh
+++ zsh/aws.zsh
@@ -2,6 +2,7 @@ alias cfn-nag='cfn_nag'
 alias cfn-nag-rules='cfn_nag_rules'
 alias cfn-nag-scan='cfn_nag_scan'
 alias isengard='isengardcli'
+alias mwk='mwinit --aea -k ~/.ssh/aws_gitlab_ecdsa.pub'
 
 if [[ $(hostname) == "bcd074a59be9.ant.amazon.com" ]]; then
   eval "$(isengardcli shell-autocomplete)"
