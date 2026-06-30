fb881d8f4d6d6545ad6430dcf19fb24677e299ce
Brian Skjerven <brian.skjerven@gmail.com>
2026-06-30 06:46:19 +0100

    Added no ask flag to brew upgrade

--- zsh/brew-update.zsh
+++ zsh/brew-update.zsh
@@ -4,13 +4,13 @@ if is_osx; then
   #Homebrew
   echo "Updating Homebrew packages"
   brew update
-  brew upgrade
+  brew upgrade -y
   brew cleanup
   brew doctor
 
   #Homebrew casks
   echo "Updating Homebrew casks"
-  brew upgrade --cask
+  brew upgrade --cask -y
   echo ""
 fi
 }
